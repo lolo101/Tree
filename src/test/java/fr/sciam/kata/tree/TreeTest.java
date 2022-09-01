c6f2ebabea8b5bf049c7d3183b47060d6bcf6aef
Alexandre Lewandowski <alexandre.lewandowski@catalina.com>
2022-09-01 23:42:25 +0200

    Ajout d'un cas de test qui devrait passer ...

--- src/test/java/fr/sciam/kata/tree/TreeTest.java
+++ src/test/java/fr/sciam/kata/tree/TreeTest.java
@@ -4,6 +4,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
+import java.util.stream.StreamSupport;
+
 class TreeTest {
     @Test
     void should_return_node_value() {
@@ -38,4 +42,28 @@ class TreeTest {
         String text = Tree.getRoot().toText();
         assertEquals("Je suis un arbre en largeur", text);
     }
+
+    @Test
+    void should_not_break_with_deep_tree() {
+        var testCase = createDeepTree();
+        String text = testCase.root.toText();
+        assertEquals(testCase.expected, text);
+    }
+
+    private TestCase createDeepTree(){
+        int i = 300_000;
+        var expectedBuilder = new StringBuilder();
+        Tree.Node firstNode = new Tree.Node(Integer.toString(i));
+        expectedBuilder.append(i);
+        do {
+            i--;
+            expectedBuilder.insert(0, " ");
+            expectedBuilder.insert(0, Integer.toString(i));
+            firstNode = new Tree.Node(Integer.toString(i), firstNode);
+        } while (i >= 1);
+        return new TestCase(firstNode, expectedBuilder.toString());
+    }
+
+    record TestCase(Tree.Node root, String expected) { }
+
 }
